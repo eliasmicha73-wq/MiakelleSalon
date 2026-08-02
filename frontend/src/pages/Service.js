4677e84d37a3ef0ea3d7195d05176ea37132305e
eliasmicha73-wq <eliasmicha73@gmail.com>
2026-08-02 06:48:11 +0300

    fix: add missing Services.css fy build

--- frontend/src/pages/Service.js
+++ frontend/src/pages/Service.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../api";
-import "../styles/Services.css";
+import "../styles/Service.css";
 
 function Services() {
   const [services, setServices] = useState([]);
